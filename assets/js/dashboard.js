6ba5c24340db8699704dfcf5e7ec80e6f4313f61
iSaint <188669077+Sanctana@users.noreply.github.com>
2026-03-29 22:33:58 +0800

    landing and dashboard

---
+++ assets/js/dashboard.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const grid = document.getElementById("cal");
+    const mon = document.getElementById("mon");
+
+    const now = new Date();
+
+    const months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+
+    mon.innerText = `${months[now.getMonth()]} ${now.getFullYear()}`;
+
+    const days = ["S", "M", "T", "W", "T", "F", "S"];
+
+    days.forEach(d => {
+        const div = document.createElement("div");
+        div.className = "cal-h";
+        div.innerText = d;
+        grid.appendChild(div);
+    });
+
+    const f = new Date(now.getFullYear(), now.getMonth(), 1).getDay();
+    const l = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+
+    for (let i = 0; i < f; i++) {
+        grid.appendChild(document.createElement("div"));
+    }
+
+    for (let i = 1; i <= l; i++) {
+        const d = document.createElement("div");
+        d.className = "cal-d";
+
+        if (i === now.getDate()) d.classList.add("act");
+
+        d.innerText = i;
+        grid.appendChild(d);
+    }
+});
